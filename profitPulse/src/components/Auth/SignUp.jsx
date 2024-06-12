b54a2c096d848cf038b86edb71a15fcf1584eefb
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-06-12 13:23:38 +0300

    Fix errors

--- profitPulse/src/components/Auth/SignUp.jsx
+++ profitPulse/src/components/Auth/SignUp.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import { SignUpUser } from '../services/Auth'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react"
+import { SignUpUser } from "../../services/Auth"
+import { useNavigate } from "react-router-dom"
 
 const SignUp = () => {
   let navigate = useNavigate()
   const [formValues, setFormValues] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   })
 
   const handleChange = (e) => {
@@ -20,15 +20,15 @@ const SignUp = () => {
     await SignUpUser({
       name: formValues.name,
       email: formValues.email,
-      password: formValues.password
+      password: formValues.password,
     })
     setFormValues({
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: ''
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     })
-    navigate('/signin')
+    navigate("/signin")
   }
 
   return (
