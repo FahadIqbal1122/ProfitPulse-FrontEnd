ab8d427b1eaf99b0007958eba95cead3a64507b2
AbeerMajeed <abeermajeed78@gmail.com>
2024-06-12 12:32:42 +0300

    dexpense and budget m-data needs test

--- profitPulse/src/components/M-Data/Budget.jsx
+++ profitPulse/src/components/M-Data/Budget.jsx
@@ -8,19 +8,25 @@ const Budget = (user) => {
 
   useEffect(() => {
     const handlePosts = async () => {
+      //const response = await fetch('http://data')
       const data = await GetPosts()
       setPosts(data)
     }
     handlePosts()
   }, [])
-  return (
+  return user ? (
     <div>
       {posts.map((post) => (
-        <div>
-          <p>{post.name}</p>
+        <div key={post.id}>
+          <h3>{post.name}</h3>
           <p>{post.limit}</p>
         </div>
       ))}
+    </div>
+  ) : (
+    <div>
+      <h3>Oops! You must be signed in to do that!</h3>
+      <button onClick={() => navigate('/signin')}>Sign In</button>
     </div>
   )
 }
