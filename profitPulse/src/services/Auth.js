cb77f4a6b6bb9f989d8c734c2b4a21692fbb0b07
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-06-12 21:14:45 +0300

    Fix Login Sign Up

--- profitPulse/src/services/Auth.js
+++ profitPulse/src/services/Auth.js
@@ -1,10 +1,10 @@
-import Client from './api'
+import Client from "./api"
 
 export const LogInUser = async (data) => {
   try {
-    const res = await Client.post('./auth/login', data)
+    const res = await Client.post("./auth/login", data)
     // Set the current signed in users token to localStorage
-    localStorage.setItem('token', res.data.token)
+    localStorage.setItem("token", res.data.token)
     return res.data.user
   } catch (error) {
     throw error
@@ -13,7 +13,7 @@ export const LogInUser = async (data) => {
 
 export const SignUpUser = async (data) => {
   try {
-    const res = await Client.post('../components/auth/signup', data)
+    const res = await Client.post("/auth/register", data)
     return res.data
   } catch (error) {
     throw error
@@ -23,7 +23,7 @@ export const SignUpUser = async (data) => {
 export const CheckSession = async () => {
   try {
     // Checks if the current token if it exists is valid
-    const res = await Client.get('./auth/session')
+    const res = await Client.get("./auth/session")
     return res.data
   } catch (error) {
     throw error
