c81615e5cc08b905dc7d0a56a68545edc00b48b8
Jellooking <jassimkhalaf14@gmail.com>
2024-06-12 10:46:26 +0300

    Auth and services work but doesntdisplay fixed app.jsx too

---
+++ profitPulse/src/components/services/Auth.js
@@ -0,0 +1,31 @@
+import Client from './api'
+
+export const LogInUser = async (data) => {
+  try {
+    const res = await Client.post('./auth/login', data)
+    // Set the current signed in users token to localStorage
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+
+export const SignUpUser = async (data) => {
+  try {
+    const res = await Client.post('./auth/signup', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CheckSession = async () => {
+  try {
+    // Checks if the current token if it exists is valid
+    const res = await Client.get('./auth/session')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
