467e583744b5af49e211e45cd2bc47bcc7a4ee5b
Afrah Mohammed <af.moali99@gmail.com>
2024-06-13 08:21:09 +0300

    test

--- profitPulse/src/App.jsx
+++ profitPulse/src/App.jsx
@@ -2,11 +2,14 @@ import ExpTrack from "./components/DashBoard"
 import IncomeTrack from "./components/DashBoard"
 import Summmary from "./components/DashBoard"
 import "./App.css"
+import Summary from "./components/DashBoard/Summary"
 
 const App = () => {
   return (
     <div>
       <IncomeTrack />
+      <ExpTrack />
+      <Summary />
     </div>
   )
 }
