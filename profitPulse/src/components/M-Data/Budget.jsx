c73655b8361adf9b116fc718f09491c3cc256658
AbeerMajeed <abeermajeed78@gmail.com>
2024-06-13 11:27:57 +0300

    Budget form added

--- profitPulse/src/components/M-Data/Budget.jsx
+++ profitPulse/src/components/M-Data/Budget.jsx
@@ -1,33 +1,79 @@
-import { GetPosts } from '../Auth'
 import { useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-
-const Budget = (user) => {
+import { useState } from 'react'
+const Budget = () => {
+  const [formValues, setFormValues] = useState({
+    name: '',
+    limit: ''
+  })
   let navigate = useNavigate()
-  const [posts, setPosts] = useState([])
-
-  useEffect(() => {
-    const handlePosts = async () => {
-      //const response = await fetch('http://data')
-      const data = await GetPosts()
-      setPosts(data)
-    }
-    handlePosts()
-  }, [])
-  return user ? (
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await createBudget({
+      name: formValues.name,
+      limit: formValues.limit
+    })
+    setFormValues({
+      name: '',
+      limit: ''
+    })
+    //navigate('/')
+  }
+  return (
     <div>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h3>{post.name}</h3>
-          <p>{post.limit}</p>
+      <div>
+        <form onSubmit={handleSubmit}></form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            onChange={handleChange}
+            name="name"
+            type="text"
+            value={formValues.name}
+            required
+          />
         </div>
-      ))}
-    </div>
-  ) : (
-    <div>
-      <h3>Oops! You must be signed in to do that!</h3>
-      <button onClick={() => navigate('/signin')}>Sign In</button>
+        <label htmlFor="limit">Limit</label>
+        <input
+          onChange={handleChange}
+          name="limit"
+          type="number"
+          value={formValues.limit}
+          required
+        />
+      </div>
+      <button disabled={!formValues.name || !formValues.limit}>
+        Add Budget
+      </button>
     </div>
   )
 }
 export default Budget
+// let navigate = useNavigate()
+// const [posts, setPosts] = useState([])
+
+// useEffect(() => {
+//   const handlePosts = async () => {
+//     //const response = await fetch('http://data')
+//     const data = await GetPosts()
+//     setPosts(data)
+//   }
+//   handlePosts()
+// }, [])
+// return user ? (
+//   <div>
+//     {posts.map((post) => (
+//       <div key={post.id}>
+//         <h3>{post.name}</h3>
+//         <p>{post.limit}</p>
+//       </div>
+//     ))}
+//   </div>
+// ) : (
+//   <div>
+//     <h3>Oops! You must be signed in to do that!</h3>
+//     <button onClick={() => navigate('/signin')}>Sign In</button>
+//   </div>
+// )
