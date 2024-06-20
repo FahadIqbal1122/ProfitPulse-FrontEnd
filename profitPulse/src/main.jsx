09af52a9eccd6aabee308083abed652a5715312d
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-06-20 13:56:22 +0300

    prepare for deploy

--- profitPulse/src/main.jsx
+++ profitPulse/src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import App from "./App.jsx"
-import { UserProvider } from "./components/Context/UserContext.jsx"
+
 createRoot(document.querySelector("#root")).render(
-  <UserProvider>
-    <Router>
-      <App />
-    </Router>
-  </UserProvider>
+  <Router>
+    <App />
+  </Router>
 )
